82720f7972fc04e1b26c8f900168307533aceada
Stiven Gaitan <johan.gaitan@mqa-bc.com>
2023-03-20 00:27:22 -0500

    cetered detail

--- lib/main.dart
+++ lib/main.dart
@@ -7,71 +7,79 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        home: ListView()
-    );
+    return MaterialApp(title: 'Flutter Demo', home: ListView());
   }
 }
 
-class ListView extends StatelessWidget{
-    List<Task> tasks = TaskFactory().generateListTask();
+class ListView extends StatelessWidget {
+  List<Task> tasks = TaskFactory().generateListTask();
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-            appBar: AppBar(
-                title: const Text("Todos")),
-            body: Container(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                  children: tasks
-                      .map((task) => GestureDetector(
-                          onTap: () => {
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => DetailView(task: task)))
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Todos")),
+        body: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+              children: tasks
+                  .map((task) => GestureDetector(
+                      onTap: () => {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        DetailView(task: task)))
                           },
-                          child: Container(
-                            height: 70,
-                            margin: const EdgeInsets.only(bottom: 10),
-                            padding: const EdgeInsets.only(left: 15, right: 15),
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                    width: 3, color: Colors.lightBlueAccent),
-                                borderRadius: BorderRadius.circular(15)),
-                            child: Row(children: [
-                              const Icon(Icons.arrow_right),
-                              Text(task.getName()),
-                              Checkbox(value: task.getStatus(), onChanged: null)
-                            ]),
-                          )))
-                      .toList()),
-            ));
-    }
+                      child: Container(
+                        height: 70,
+                        margin: const EdgeInsets.only(bottom: 10),
+                        padding: const EdgeInsets.only(left: 15, right: 15),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                width: 3, color: Colors.lightBlueAccent),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Row(children: [
+                          const Icon(Icons.arrow_right),
+                          Text(task.getName()),
+                          Checkbox(value: task.getStatus(), onChanged: null)
+                        ]),
+                      )))
+                  .toList()),
+        ));
+  }
 }
 
+class DetailView extends StatelessWidget {
+  const DetailView({super.key, required this.task});
 
-class DetailView extends StatelessWidget{
-    const DetailView({super.key, required this.task});
+  final Task task;
 
-    final Task task;
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(title: Text(task.getName())),
-        body: Center(child: 
-          Column(children: [
-            Text(task.getName()),
-            Text(task.getInitialDate()),
-            Text(task.getEndDate()),
-            Text(task.getStatus().toString()),
-          ])
-        )
-      );
-    }
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                  child: Center(
+                      child: Container(
+                          height: 80,
+                          width: 220,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  width: 3, color: Colors.lightBlueAccent),
+                              borderRadius: BorderRadius.circular(15)),
+                          child: Column(children: [
+                            Text(task.getName()),
+                            Text(task.getInitialDate()),
+                            Text(task.getEndDate()),
+                            Text(task.getStatus().toString()),
+                          ]))))
+            ]));
+  }
 }
